fcc3d3e950083718dfbc9624c10a5942f685a8fe
Katie Palof <katie.palof@alaska.gov>
2017-10-13 15:19:15 -0800

    currentyr added to this function to indicate the model year, i.e. this year is 2017

--- code/functions.R
+++ code/functions.R
@@ -12,11 +12,12 @@ theme_set(theme_bw(base_size=12,base_family='Times New Roman')+
                   panel.grid.minor = element_blank()))
 
 # functions ----
-f.regional.fig <- function(x, region=NULL, startyr, endyr, closures = NULL){
+f.regional.fig <- function(x, region=NULL, startyr, endyr, currentyr, closures = NULL){
   # x = data
   # region = the region of interest
   # startyr = starting year to calculate long term average 
   # endyr = end year to calculate long term average
+  # currentyr = the current year used to generate the biomass estimates
   # closures = file with open or closed and year 
   
   if(missing(region) && missing(closures)){
@@ -35,13 +36,13 @@ f.regional.fig <- function(x, region=NULL, startyr, endyr, closures = NULL){
       geom_line(aes(color = type, group = type)) +
       scale_colour_manual(name = "", values = c("grey1", "grey1")) +
       scale_shape_manual(name = "", values = c(16, 1)) +
-      ylim(0,1500000) + ggtitle("Survey areas 2017 Model") +
+      ylim(0,1500000) + ggtitle(paste0("Survey areas ", currentyr, " Model, ", startyr, "-", endyr, " average" )) +
       ylab("Biomass (lbs)") + xlab("") +
       theme(plot.title = element_text(hjust =0.5)) +
       scale_x_continuous(breaks = seq(1979, 2017, by =5)) +
       theme(legend.position = c(0.8,0.7)) +
-      geom_hline(yintercept = out$legal, color = "grey1") +
-      geom_hline(yintercept = out$mature, color = "grey1", lty = 4) 
+      geom_hline(yintercept = out$legal, color = "grey1", lty = 4) +
+      geom_hline(yintercept = out$mature, color = "grey1") 
       ggsave(paste0("results/regional_biomass", endyr, ".png"), plot = last_plot(), device="png",
              dpi=300, height=5.0, width=7.55, units="in")
   } else if(missing(region) && !missing(closures)){ 
@@ -63,13 +64,13 @@ f.regional.fig <- function(x, region=NULL, startyr, endyr, closures = NULL){
       scale_colour_manual(name = "", 
                           values = c("grey1", "gray55", "grey1", "red")) +
       scale_shape_manual(name = "", values = c(16, 1, 20)) +
-      ylim(0,1500000) +ggtitle("Survey areas 2017 Model") + 
+      ylim(0,1500000) + ggtitle(paste0("Survey areas ", currentyr, " Model, ", startyr, "-", endyr, " average" )) +
       ylab("Biomass (lbs)")+ xlab("") +
       theme(plot.title = element_text(hjust =0.5)) +
       scale_x_continuous(breaks = seq(1979, 2017, by =5)) +
       theme(legend.position = c(0.8,0.7)) +
-      geom_hline(yintercept = out$legal, color = "grey1") +
-      geom_hline(yintercept = out$mature, color = "grey1", lty = 4) 
+      geom_hline(yintercept = out$legal, color = "grey1", lty = 4) +
+      geom_hline(yintercept = out$mature, color = "grey1") 
       ggsave(paste0("results/regional_open_closed", endyr, ".png"), plot = last_plot(), device="png",
              dpi=300, height=5.0, width=7.55, units="in")
   } else{
@@ -87,13 +88,13 @@ f.regional.fig <- function(x, region=NULL, startyr, endyr, closures = NULL){
       geom_line(aes(color = type, group = type)) +
       scale_colour_manual(name = "", values = c("grey1", "grey1")) +
       scale_shape_manual(name = "", values = c(16, 1)) +
-      ggtitle(paste0("2017 Model ", y, " survey area")) +
+      ggtitle(paste0( currentyr, " Model ", y, " survey area, ", startyr, "-", endyr, " average")) +
       ylab("Biomass (lbs)") + xlab("") +
       theme(plot.title = element_text(hjust =0.5)) +
-      scale_x_continuous(breaks = seq(1979, 2017, by =2)) +
+      scale_x_continuous(breaks = seq(1979, 2017, by =5)) +
       theme(legend.position = c(0.8,0.7)) +
-      geom_hline(yintercept = out$legal, color = "grey1") +
-      geom_hline(yintercept = out$mature, color = "grey1", lty = 4) 
+      geom_hline(yintercept = out$legal, color = "grey1", lty = 4) +
+      geom_hline(yintercept = out$mature, color = "grey1") 
       ggsave(paste0("results/", y, endyr, ".png"), plot = last_plot(), device="png",
              dpi=300, height=5.0, width=7.55, units="in")
   }
