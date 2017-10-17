8ddc22ae3c407eba09cff7bcd137cb11e4ce1769
Katie Palof <katie.palof@alaska.gov>
2017-10-17 11:34:31 -0800

    tables for average and threshold values

--- code/mature_biomass_threshold_figures.R
+++ code/mature_biomass_threshold_figures.R
@@ -48,7 +48,8 @@ f.regional.fig(biomass_17, startyr, endyr = 2017, currentyr, closures = fishery.
 ### area figures  ----------------
 ## Pybus ---------------
 f.regional.table(biomass_17, region = 'Pybus', startyr, endyr)
-f.regional.thresholds(biomass_17, region = 'Pybus', startyr, endyr)
+p1 <- f.regional.thresholds(biomass_17, region = 'Pybus', startyr, endyr)
+p2 <- f.regional.thresholds(biomass_17, region = 'Pybus', startyr, endyr = 2017)
 
 f.regional.fig(biomass_17, region = 'Pybus', startyr, endyr, currentyr)
 f.regional.fig(biomass_17, region = 'Pybus', startyr, endyr = 2017, currentyr)
@@ -57,7 +58,9 @@ f.threshold.fig(biomass_17, region = 'Pybus', startyr, endyr = 2017, currentyr)
 
 ## Gambier ---------------
 f.regional.table(biomass_17, region = 'Gambier', startyr, endyr)
-f.regional.thresholds(biomass_17, region = 'Gambier', startyr, endyr)
+
+g1 <- f.regional.thresholds(biomass_17, region = 'Gambier', startyr, endyr)
+g2 <- f.regional.thresholds(biomass_17, region = 'Gambier', startyr, endyr = 2017)
 
 f.regional.fig(biomass_17, region = 'Gambier', startyr, endyr, currentyr)
 f.regional.fig(biomass_17, region = 'Gambier', startyr, endyr = 2017, currentyr)
@@ -66,7 +69,9 @@ f.threshold.fig(biomass_17, region = 'Gambier', startyr, endyr = 2017, currentyr
 
 ## Seymour ---------------
 f.regional.table(biomass_17, region = 'Seymour', startyr, endyr)
-f.regional.thresholds(biomass_17, region = 'Seymour', startyr, endyr)
+
+s1 <- f.regional.thresholds(biomass_17, region = 'Seymour', startyr, endyr)
+s2 <- f.regional.thresholds(biomass_17, region = 'Seymour', startyr, endyr = 2017)
 
 f.regional.fig(biomass_17, region = 'Seymour', startyr, endyr, currentyr)
 f.regional.fig(biomass_17, region = 'Seymour', startyr, endyr = 2017, currentyr)
@@ -75,7 +80,9 @@ f.threshold.fig(biomass_17, region = 'Seymour', startyr, endyr = 2017, currentyr
 
 ## Lynn Canal ---------------
 f.regional.table(biomass_17, region = 'Lynn Canal', startyr, endyr)
-f.regional.thresholds(biomass_17, region = 'Lynn Canal', startyr, endyr)
+
+l1 <- f.regional.thresholds(biomass_17, region = 'Lynn Canal', startyr, endyr)
+l2 <- f.regional.thresholds(biomass_17, region = 'Lynn Canal', startyr, endyr = 2017)
 
 f.regional.fig(biomass_17, region = 'Lynn Canal', startyr, endyr, currentyr)
 f.regional.fig(biomass_17, region = 'Lynn Canal', startyr, endyr = 2017, currentyr)
@@ -84,8 +91,10 @@ f.threshold.fig(biomass_17, region = 'Lynn Canal', startyr, endyr = 2017, curren
 
 ## Juneau ---------------
 f.regional.table(biomass_17, region = 'Juneau', startyr, endyr)
-f.regional.thresholds(biomass_17, region = 'Juneau', startyr, endyr)
 
+j1 <- f.regional.thresholds(biomass_17, region = 'Juneau', startyr, endyr)
+j2 <- f.regional.thresholds(biomass_17, region = 'Juneau', startyr, endyr = 2017)
+                            
 f.regional.fig(biomass_17, region = 'Juneau', startyr, endyr, currentyr)
 f.regional.fig(biomass_17, region = 'Juneau', startyr, endyr = 2017, currentyr)
 f.threshold.fig(biomass_17, region = 'Juneau', startyr, endyr, currentyr)
@@ -93,7 +102,9 @@ f.threshold.fig(biomass_17, region = 'Juneau', startyr, endyr = 2017, currentyr)
 
 ## Peril ---------------
 f.regional.table(biomass_17, region = 'Peril', startyr, endyr)
-f.regional.thresholds(biomass_17, region = 'Peril', startyr, endyr)
+
+ps1 <- f.regional.thresholds(biomass_17, region = 'Peril', startyr, endyr)
+ps2 <- f.regional.thresholds(biomass_17, region = 'Peril', startyr, endyr = 2017)
 
 f.regional.fig(biomass_17, region = 'Peril', startyr, endyr, currentyr)
 f.regional.fig(biomass_17, region = 'Peril', startyr, endyr = 2017, currentyr)
@@ -103,7 +114,9 @@ f.threshold.fig(biomass_17, region = 'Peril', startyr, endyr = 2017, currentyr)
 
 ## Excursion ---------------
 f.regional.table(biomass_17, region = 'Excursion', startyr, endyr)
-f.regional.thresholds(biomass_17, region = 'Excursion', startyr, endyr)
+
+e1 <- f.regional.thresholds(biomass_17, region = 'Excursion', startyr, endyr)
+e2 <- f.regional.thresholds(biomass_17, region = 'Excursion', startyr, endyr = 2017)
 
 f.regional.fig(biomass_17, region = 'Excursion', startyr, endyr, currentyr)
 f.regional.fig(biomass_17, region = 'Excursion', startyr, endyr = 2017, currentyr)
@@ -114,8 +127,20 @@ f.threshold.fig(biomass_17, region = 'Excursion', startyr, endyr = 2017, current
 # table summary of averages and thresholds ---------
 
 t1 %>% 
-  select(legal, mature, p50mat, end) -> t2007
+  bind_rows(p1) %>% 
+  bind_rows(s1) %>% 
+  bind_rows(l1) %>% 
+  bind_rows(j1) %>% 
+  bind_rows(ps1) %>% 
+  bind_rows(e1) %>% 
+  select(legal, mature, p50mat, end, survey_area) -> t2007
 
 
-
-
+t2 %>% 
+  bind_rows(p2) %>% 
+  bind_rows(s2) %>% 
+  bind_rows(l2) %>% 
+  bind_rows(j2) %>% 
+  bind_rows(ps2) %>% 
+  bind_rows(e2) %>% 
+  select(legal, mature, p50mat, end, survey_area) -> t2017
